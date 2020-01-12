3bc92e58494d4e262c7385b8a2aa636a416fa2cc
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-01-12 14:13:34 -0400

    get task by id by user

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -38,8 +38,11 @@ export class TasksController {
   }
 
   @Get("/:id")
-  getTaskById(@Param("id", ParseIntPipe) id: number): Promise<Task> {
-    return this.taskService.getTaskById(id);
+  getTaskById(
+    @Param("id", ParseIntPipe) id: number,
+    @GetUser() user: User
+  ): Promise<Task> {
+    return this.taskService.getTaskById(id, user);
   }
 
   @Post()
@@ -51,13 +54,13 @@ export class TasksController {
     return this.taskService.createTask(createTaskDto, user);
   }
 
-  @Patch("/:id/status")
-  updateTaskStatus(
-    @Param("id", ParseIntPipe) id: number,
-    @Body("status", TaskStatusValidationPipe) status: TaskStatus
-  ): Promise<Task> {
-    return this.taskService.updateTaskStatus(id, status);
-  }
+  // @Patch("/:id/status")
+  // updateTaskStatus(
+  //   @Param("id", ParseIntPipe) id: number,
+  //   @Body("status", TaskStatusValidationPipe) status: TaskStatus
+  // ): Promise<Task> {
+  //   return this.taskService.updateTaskStatus(id, status);
+  // }
 
   @Delete("/:id")
   deleteTask(@Param("id", ParseIntPipe) id: number): Promise<void> {
